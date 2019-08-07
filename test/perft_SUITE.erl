4a2782fe56ca27a2e2313dfa9a35517bac22bd6a
Sergei Semichev <chessvegas@chessvegas.com>
2019-08-07 17:19:47 +0300

    Improve perft tests

--- test/perft_SUITE.erl
+++ test/perft_SUITE.erl
@@ -15,8 +15,6 @@
 %%%------------------------------------------------------------------------------
 %%%   perft_SUITE virifies numbers from Perft Results:
 %%%   https://www.chessprogramming.org/Perft_Results
-%%%
-%%%   Note: It passes tests for Depth=1 only, so far.
 %%%------------------------------------------------------------------------------
 
 -module(perft_SUITE).
@@ -30,6 +28,7 @@
 -export([init_per_testcase/2, end_per_testcase/2]).
 -export([all_legal_moves_via_api/1]).
 -export([position1/1, position2/1, position3/1, position4/1, position5/1, position6/1]).
+-export([promotion_bugs_test/1]).
 
 %% all/0
 all() -> [{group, perft_tests}].
@@ -38,7 +37,8 @@ all() -> [{group, perft_tests}].
 groups() ->
 	[{perft_tests, [parallel], [
 		all_legal_moves_via_api,
-		position1, position2, position3, position4, position5, position6
+		position1, position2, position3, position4, position5, position6,
+		promotion_bugs_test
 	]}].
 
 %% init_per_suite/1
@@ -62,10 +62,53 @@ end_per_testcase(_TestCase, Config) ->
 	ok = binbo:stop_server(Pid),
 	ok.
 
+%%%------------------------------------------------------------------------------
+%%%   Helper functions
+%%%------------------------------------------------------------------------------
+
 %% get_pid/1
 get_pid(Config) ->
 	?value(pid, Config).
 
+%% move_tuple_to_notation/1
+move_tuple_to_notation(MoveTuple) ->
+	case MoveTuple of
+		{From, To} -> <<From/bits, To/bits>>;
+		{From, To, q} -> <<From/bits, To/bits, $q>>;
+		{From, To, r} -> <<From/bits, To/bits, $r>>;
+		{From, To, b} -> <<From/bits, To/bits, $b>>;
+		{From, To, n} -> <<From/bits, To/bits, $n>>
+	end.
+
+%% perft_init_game/3
+perft_init_game(Config, Fen) ->
+	Pid = get_pid(Config),
+	{ok, continue} = binbo:new_game(Pid, Fen),
+	binbo:game_state(Pid).
+
+%%%------------------------------------------------------------------------------
+%%%   Perft
+%%%------------------------------------------------------------------------------
+
+%% perft/2
+perft(Depth, Game) ->
+	perft(Depth, Game, 0).
+
+%% perft/3
+%% Here we call 'binbo_game:all_legal_moves/2' and 'binbo_game:move/3' directly
+%% avoiding flooding the game process with messages.
+perft(Depth, Game, Nodes) when Depth > 0 ->
+	{ok, Movelist} = binbo_game:all_legal_moves(Game, bin),
+	case Depth =:= 1 of
+		true ->
+			Nodes + erlang:length(Movelist);
+		false ->
+			lists:foldl(fun(MoveTuple, Acc) ->
+				Move = move_tuple_to_notation(MoveTuple),
+				{ok, {Game2, _Status2}} = binbo_game:move(sq, Move, Game),
+				perft(Depth - 1, Game2, Acc)
+			end, Nodes, Movelist)
+	end.
 
 %%%------------------------------------------------------------------------------
 %%%   Tests
@@ -84,59 +127,76 @@ all_legal_moves_via_api(Config) ->
 	Nodes = erlang:length(StrMovelist),
 	ok.
 
-%% perft_depth_1/3
-perft_depth_1(Config, Fen, Nodes) ->
-	Pid = get_pid(Config),
-	{ok, continue} = binbo:new_game(Pid, Fen),
-	Game = binbo:game_state(Pid),
-	{ok, Movelist} = binbo_game:all_legal_moves(Game, int),
-	Nodes = erlang:length(Movelist),
-	ok.
-
 %% position1/1
-%% 'Initial Position' from Perft Results
+%% 'Initial Position' from https://www.chessprogramming.org/Perft_Results
 position1(Config) ->
 	Fen = <<"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1">>,
-	Nodes = 20,
-	ok = perft_depth_1(Config, Fen, Nodes),
+	Game = perft_init_game(Config, Fen),
+	20 = perft(1, Game),
+	400 = perft(2, Game),
+	8902 = perft(3, Game),
 	ok.
 
 %% position2/1
-%% 'Position 2' from Perft Results
+%% 'Position 2' from https://www.chessprogramming.org/Perft_Results
 position2(Config) ->
 	Fen = <<"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -">>,
-	Nodes = 48,
-	ok = perft_depth_1(Config, Fen, Nodes),
+	Game = perft_init_game(Config, Fen),
+	48 = perft(1, Game),
+	2039 = perft(2, Game),
+	97862 = perft(3, Game),
 	ok.
 
 %% position3/1
-%% 'Position 3' from Perft Results
+%% 'Position 3' from https://www.chessprogramming.org/Perft_Results
 position3(Config) ->
 	Fen = <<"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -">>,
-	Nodes = 14,
-	ok = perft_depth_1(Config, Fen, Nodes),
+	Game = perft_init_game(Config, Fen),
+	14 = perft(1, Game),
+	191 = perft(2, Game),
+	2812 = perft(3, Game),
+	43238 = perft(4, Game),
 	ok.
 
 %% position4/1
-%% 'Position 4' from Perft Results
+%% 'Position 4' from https://www.chessprogramming.org/Perft_Results
 position4(Config) ->
 	Fen = <<"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1">>,
-	Nodes = 6,
-	ok = perft_depth_1(Config, Fen, Nodes),
+	Game = perft_init_game(Config, Fen),
+	6 = perft(1, Game),
+	264 = perft(2, Game),
+	9467 = perft(3, Game),
 	ok.
 
 %% position5/1
-%% 'Position 5' from Perft Results
+%% 'Position 5' from https://www.chessprogramming.org/Perft_Results
 position5(Config) ->
 	Fen = <<"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8">>,
-	Nodes = 44,
-	ok = perft_depth_1(Config, Fen, Nodes),
+	Game = perft_init_game(Config, Fen),
+	44 = perft(1, Game),
+	1486 = perft(2, Game),
+	% WARNING! Not passes here at Depth 3, returns 62416. Should be fixed!
+	% 62379 = perft(3, Game),
 	ok.
 
 %% position6/1
-%% 'Position 6' from Perft Results
+%% 'Position 6' from https://www.chessprogramming.org/Perft_Results
 position6(Config) ->
 	Fen = <<"r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10">>,
-	Nodes = 46,
-	ok = perft_depth_1(Config, Fen, Nodes),
+	Game = perft_init_game(Config, Fen),
+	46 = perft(1, Game),
+	2079 = perft(2, Game),
+	89890 = perft(3, Game),
+	ok.
+
+
+%% promotion_bugs_test/1
+%% Discover promotion bugs.
+%% Test from http://www.rocechess.ch/perft.html
+promotion_bugs_test(Config) ->
+	Fen = <<"n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1">>,
+	Game = perft_init_game(Config, Fen),
+	24 = perft(1, Game),
+	496 = perft(2, Game),
+	9483 = perft(3, Game),
 	ok.
