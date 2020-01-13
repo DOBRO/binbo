36430473ee4242e47bb51f7ba65a7f291761e92c
Sergei Semichev <chessvegas@chessvegas.com>
2020-01-13 19:10:08 +0300

    Simplify convertion of square bitboard to index

--- src/binbo_bb.erl
+++ src/binbo_bb.erl
@@ -95,72 +95,9 @@ file_bb(Idx) ->
 %% to_index/1
 -spec to_index(sq_bb()) -> sq_idx().
 to_index(SqBB) ->
-	(case SqBB of
-		1 -> 0;
-		2 -> 1;
-		4 -> 2;
-		8 -> 3;
-		16 -> 4;
-		32 -> 5;
-		64 -> 6;
-		128 -> 7;
-		256 -> 8;
-		512 -> 9;
-		1024 -> 10;
-		2048 -> 11;
-		4096 -> 12;
-		8192 -> 13;
-		16384 -> 14;
-		32768 -> 15;
-		65536 -> 16;
-		131072 -> 17;
-		262144 -> 18;
-		524288 -> 19;
-		1048576 -> 20;
-		2097152 -> 21;
-		4194304 -> 22;
-		8388608 -> 23;
-		16777216 -> 24;
-		33554432 -> 25;
-		67108864 -> 26;
-		134217728 -> 27;
-		268435456 -> 28;
-		536870912 -> 29;
-		1073741824 -> 30;
-		2147483648 -> 31;
-		4294967296 -> 32;
-		8589934592 -> 33;
-		17179869184 -> 34;
-		34359738368 -> 35;
-		68719476736 -> 36;
-		137438953472 -> 37;
-		274877906944 -> 38;
-		549755813888 -> 39;
-		1099511627776 -> 40;
-		2199023255552 -> 41;
-		4398046511104 -> 42;
-		8796093022208 -> 43;
-		17592186044416 -> 44;
-		35184372088832 -> 45;
-		70368744177664 -> 46;
-		140737488355328 -> 47;
-		281474976710656 -> 48;
-		562949953421312 -> 49;
-		1125899906842624 -> 50;
-		2251799813685248 -> 51;
-		4503599627370496 -> 52;
-		9007199254740992 -> 53;
-		18014398509481984 -> 54;
-		36028797018963968 -> 55;
-		72057594037927936 -> 56;
-		144115188075855872 -> 57;
-		288230376151711744 -> 58;
-		576460752303423488 -> 59;
-		1152921504606846976 -> 60;
-		2305843009213693952 -> 61;
-		4611686018427387904 -> 62;
-		9223372036854775808 -> 63
-	end).
+	% The bitboard of square is a single populated bitboard, a power of two value.
+	% Therefore, the index of square is equal to base-2 logarithm of the bitboard.
+	erlang:trunc(math:log2(SqBB)).
 
 %% to_file/1
 -spec to_file(sq_bb()) -> binbo_board:file().
@@ -171,7 +108,7 @@ to_file(SqBB) ->
 %% to_index_list/1
 -spec to_index_list(bb()) -> [sq_idx()].
 to_index_list(BB) ->
-	to_index_list(BB, ?A1_IDX, []).
+	to_index_list(BB, []).
 
 %% to_notation/1
 -spec to_notation(sq_bb()) -> binbo_board:square_notation().
@@ -355,19 +292,16 @@ bb_or([BB|Tail], AccBB) ->
 	bb_or(Tail, AccBB bor BB).
 
 
-%% to_index_list/3
--spec to_index_list(bb(), sq_idx(), [sq_idx()]) -> [sq_idx()].
-to_index_list(0, _, List) ->
+%% to_index_list/2
+-spec to_index_list(bb(), [sq_idx()]) -> [sq_idx()].
+to_index_list(0, List) ->
 	% We don't care about the order of indices.
 	% So, it's not necessary to reverse the list.
 	% lists:reverse(List);
 	List;
-to_index_list(BB, Idx, List) ->
-	Idx2 = Idx + 1,
-	case (BB band 1) of
-		0 -> to_index_list(BB bsr 1, Idx2, List);
-		_ -> to_index_list(BB bsr 1, Idx2, [Idx |List])
-	end.
+to_index_list(BB, List) ->
+	Idx = to_index(BB band (-BB)), % the index of the least significant bit (LSB)
+	to_index_list(BB band (BB - 1), [Idx | List]). % reset LSB and continue
 
 
 %% sliding_attacks_bb/3
