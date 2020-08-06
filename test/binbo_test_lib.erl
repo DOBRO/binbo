dfb0c4c8f8b6fbe7a277a7fdb72bb9455cc8d706
Sergei Semichev <chessvegas@chessvegas.com>
2020-08-06 15:37:28 +0300

    Add binbo_test_lib.erl

---
+++ test/binbo_test_lib.erl
@@ -0,0 +1,38 @@
+-module(binbo_test_lib).
+
+-export([all_group_testcases_exported/1]).
+
+
+%% all_group_testcases_exported
+all_group_testcases_exported(Mod) ->
+	Exports = Mod:module_info(exports),
+	Groups = Mod:groups(),
+	all_group_testcases_exported(Groups, Exports).
+
+
+%%%------------------------------------------------------------------------------
+%%%   Internal functions
+%%%------------------------------------------------------------------------------
+
+%% all_group_testcases_exported/2
+all_group_testcases_exported([], _Exports) ->
+	ok;
+all_group_testcases_exported([{GroupName, _Opts, TestCases} | Tail], Exports) ->
+	case group_testcases_exported(TestCases, Exports) of
+		ok ->
+			all_group_testcases_exported(Tail, Exports);
+		{error, Fun} ->
+			{error, {not_exported, GroupName, Fun}}
+	end.
+
+
+%% group_testcases_exported/2
+group_testcases_exported([], _Exports) ->
+	ok;
+group_testcases_exported([Fun | Tail], Exports) ->
+	case lists:keyfind(Fun, 1, Exports) of
+		{Fun, 1} ->
+			group_testcases_exported(Tail, Exports);
+		false ->
+			{error, Fun}
+	end.
