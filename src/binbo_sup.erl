18fd480def8a8ea4a415101f6164064fc7f7c0fb
Sergei Semichev <chessvegas@chessvegas.com>
2021-01-26 15:12:34 +0300

    Welcome to 2021

--- src/binbo_sup.erl
+++ src/binbo_sup.erl
@@ -1,4 +1,4 @@
-%% Copyright (c) 2019-2020, Sergei Semichev <chessvegas@chessvegas.com>. All Rights Reserved.
+%% Copyright (c) 2019-2021, Sergei Semichev <chessvegas@chessvegas.com>. All Rights Reserved.
 %%
 %% Licensed under the Apache License, Version 2.0 (the "License");
 %% you may not use this file except in compliance with the License.
