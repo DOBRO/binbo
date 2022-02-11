827bd7b831bad1ae5cc1b01c21d2fda292bc5087
Sergei Semichev <chessvegas@chessvegas.com>
2022-02-11 19:45:16 +0300

    Update binbo_uci_connection

--- src/binbo_uci_connection.erl
+++ src/binbo_uci_connection.erl
@@ -14,14 +14,18 @@
 
 -module(binbo_uci_connection).
 
--export([connect/1, disconnect/1]).
+-export([connect/1, connect/3, disconnect/1]).
 -export([send_command/2]).
 
 %%%------------------------------------------------------------------------------
 %%%   Types
 %%%------------------------------------------------------------------------------
--type socket_info() :: {erlang, port()}.
 -type engine_path() :: binary() | string().
+-type tcp_host() :: inet:socket_address() | inet:hostname().
+-type tcp_port() :: inet:port_number().
+-type tcp_socket() :: inet:socket().
+
+-type socket_info() :: {erlang, port()} | {gen_tcp, tcp_socket()}.
 
 -export_type([socket_info/0, engine_path/0]).
 
@@ -31,12 +35,23 @@
 %%%------------------------------------------------------------------------------
 
 %% connect/1
--spec connect(engine_path()) -> {ok, socket_info()} | {error, any()}.
+-spec connect(engine_path()) -> {ok, {erlang, port()}} | {error, any()}.
 connect(EnginePath) when is_list(EnginePath) orelse is_binary(EnginePath) ->
     try erlang:open_port({spawn_executable, EnginePath}, [binary, stream]) of
         Port -> {ok, {erlang, Port}}
     catch
         _:Reason -> {error, Reason}
+    end.
+
+%% connect/3
+-spec connect(tcp_host(), tcp_port(), timeout()) -> {ok, {gen_tcp, tcp_socket()}} | {error, timeout | inet:posix()}.
+connect(Host, Port, Timeout) ->
+    Opts = [binary, {active, true}, {packet, raw}],
+    case gen_tcp:connect(Host, Port, Opts, Timeout) of
+        {ok, Socket} ->
+            {ok, {gen_tcp, Socket}};
+        {error, Reason} ->
+            {error, Reason}
     end.
 
 %% disconnect/1
@@ -54,6 +69,10 @@ disconnect({erlang, Port} = SocketInfo) ->
             end
     end,
     ok;
+disconnect({gen_tcp, Socket} = SocketInfo) ->
+    _ = send_command(SocketInfo, "quit"),
+    _ = gen_tcp:close(Socket),
+    ok;
 disconnect(undefined) ->
     ok.
 
@@ -69,6 +88,11 @@ send_command(SocketInfo, Command) ->
 
 %% send/2
 -spec send(socket_info(), iodata()) -> ok.
-send({erlang, Port}, IoData) ->
-    _ = erlang:port_command(Port, IoData),
+send(SocketInfo, IoData) ->
+    _ = case SocketInfo of
+        {erlang, Port} ->
+            erlang:port_command(Port, IoData);
+        {gen_tcp, Socket} ->
+            gen_tcp:send(Socket, IoData)
+    end,
     ok.
